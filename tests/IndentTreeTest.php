a3ba42b389e1601cf5d9bf7239df81ab53fa154f
esinger <esinger@qq.com>
2020-05-31 14:47:01 +0800

    Modified configuration.

--- tests/IndentTreeTest.php
+++ tests/IndentTreeTest.php
@@ -49,29 +49,29 @@ class IndentTreeTest extends TestCase
     public function testIndentData()
     {
         $data = [
-            ['id' => 1, 'name' => '1-1', 'parent_id' => 0, 'indent_tree' => [
-                'tab' => '├', 'depth' => 1, 'parent_map' => [1], 'children_map' => [1, 4, 5, 7, 8], 'tab_name' => '├ 1-1',
+            ['id' => 1, 'name' => '1-1', 'parent_id' => 0, 'tree' => [
+                'tab' => '├', 'depth' => 1, 'parents' => [1], 'children' => [1, 4, 5, 7, 8], 'name' => '├ 1-1',
             ]],
-            ['id' => 4, 'name' => '2-1', 'parent_id' => 1, 'indent_tree' => [
-                'tab' => '│├', 'depth' => 2, 'parent_map' => [1, 4], 'children_map' => [4], 'tab_name' => '│├ 2-1',
+            ['id' => 4, 'name' => '2-1', 'parent_id' => 1, 'tree' => [
+                'tab' => '│├', 'depth' => 2, 'parents' => [1, 4], 'children' => [4], 'name' => '│├ 2-1',
             ]],
-            ['id' => 5, 'name' => '2-2', 'parent_id' => 1, 'indent_tree' => [
-                'tab' => '│└', 'depth' => 2, 'parent_map' => [1, 5], 'children_map' => [5, 7, 8], 'tab_name' => '│└ 2-2',
+            ['id' => 5, 'name' => '2-2', 'parent_id' => 1, 'tree' => [
+                'tab' => '│└', 'depth' => 2, 'parents' => [1, 5], 'children' => [5, 7, 8], 'name' => '│└ 2-2',
             ]],
-            ['id' => 7, 'name' => '3-1', 'parent_id' => 5, 'indent_tree' => [
-                'tab' => '│　└', 'depth' => 3, 'parent_map' => [1, 5, 7], 'children_map' => [7, 8], 'tab_name' => '│　└ 3-1',
+            ['id' => 7, 'name' => '3-1', 'parent_id' => 5, 'tree' => [
+                'tab' => '│　└', 'depth' => 3, 'parents' => [1, 5, 7], 'children' => [7, 8], 'name' => '│　└ 3-1',
             ]],
-            ['id' => 8, 'name' => '4-1', 'parent_id' => 7, 'indent_tree' => [
-                'tab' => '│　　└', 'depth' => 4, 'parent_map' => [1, 5, 7, 8], 'children_map' => [8], 'tab_name' => '│　　└ 4-1',
+            ['id' => 8, 'name' => '4-1', 'parent_id' => 7, 'tree' => [
+                'tab' => '│　　└', 'depth' => 4, 'parents' => [1, 5, 7, 8], 'children' => [8], 'name' => '│　　└ 4-1',
             ]],
-            ['id' => 2, 'name' => '1-2', 'parent_id' => 0, 'indent_tree' => [
-                'tab' => '├', 'depth' => 1, 'parent_map' => [2], 'children_map' => [2, 6], 'tab_name' => '├ 1-2',
+            ['id' => 2, 'name' => '1-2', 'parent_id' => 0, 'tree' => [
+                'tab' => '├', 'depth' => 1, 'parents' => [2], 'children' => [2, 6], 'name' => '├ 1-2',
             ]],
-            ['id' => 6, 'name' => '2-3', 'parent_id' => 2, 'indent_tree' => [
-                'tab' => '│└', 'depth' => 2, 'parent_map' => [2, 6], 'children_map' => [6], 'tab_name' => '│└ 2-3',
+            ['id' => 6, 'name' => '2-3', 'parent_id' => 2, 'tree' => [
+                'tab' => '│└', 'depth' => 2, 'parents' => [2, 6], 'children' => [6], 'name' => '│└ 2-3',
             ]],
-            ['id' => 3, 'name' => '1-3', 'parent_id' => 0, 'indent_tree' => [
-                'tab' => '└', 'depth' => 1, 'parent_map' => [3], 'children_map' => [3], 'tab_name' => '└ 1-3',
+            ['id' => 3, 'name' => '1-3', 'parent_id' => 0, 'tree' => [
+                'tab' => '└', 'depth' => 1, 'parents' => [3], 'children' => [3], 'name' => '└ 1-3',
             ]],
         ];
         $tree = new IndentTree();
